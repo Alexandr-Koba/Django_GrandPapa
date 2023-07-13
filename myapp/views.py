2bbf60a52de89d7a4bc35741fd2b65733a6ff355
AlexandrKoba <babakoba@yandex.ru>
2023-07-14 03:12:36 +0700

    "Koba Commit Database finish"

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from  myapp.models import Worker
 def index_page(request):
 
+    # ЗАПИСЬ НОВОГО ОБЬЕКТА В БД:
+    #new_worker = Worker(name='Алексей', second_name='Путин', salary=350000)
+    #new_worker.save()
+    '''
+    в одну строку
+    Worker.objects.create(name='Алексей', second_name='Путин', salary=350000)
+    '''
+
+    #worker_to_change = Worker.objects.get(id=5)
+    #worker_to_change.salary = 400000
+    #worker_to_change.save()
+    '''
+    в одну строку: Worker.objects.filter(id=5).update(name='Дима')
+    '''
+
+    #Worker.objects.get(id=5).delete()
+
     all_worker = Worker.objects.all() # Получить все данные из таблицы Worker.
-    print(all_worker)
 
     worker_filtered = Worker.objects.filter(salary=80000) # Получить данные по фильтру.
     print(f'80т.руб. получает: {worker_filtered}')
