5b53bc1f643eee798b7eb4456f3df226f2ee8fe5
AlexandrKoba <babakoba@yandex.ru>
2023-07-14 02:25:06 +0700

    "Koba Commit Database 2"

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,6 @@ class Worker(models.Model):
     name = models.CharField(max_length=20, blank=False)
     second_name = models.CharField(max_length=35, blank=False)
     salary = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.second_name} {self.name}' # Обьекты будут называться по фамилии в Админке.
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from  myapp.models import Worker
 def index_page(request):
+
+    all_worker = Worker.objects.all() # Получить все данные из таблицы Worker.
+    print(all_worker)
+
+    worker_filtered = Worker.objects.filter(salary=80000) # Получить данные по фильтру.
+    print(f'80т.руб. получает: {worker_filtered}')
+
+    for i in all_worker:
+        print(f'Фамилия: {i.second_name}, Имя: {i.name}, Зарплата: {i.salary}, Номер: {i.id}')
+
     return render(request, 'index.html')
 
 def about_page(request):
